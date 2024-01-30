365318655fe0212c4c55363132905c3cca108665
igorcampos-dev <icampos.developer@yahoo.com>
2024-01-29 23:56:04 -0300

    FIX: correcoes de implementacoes e classes

--- src/main/java/com/nexus/mail/properties/MailProperties.java
+++ src/main/java/com/nexus/mail/properties/MailProperties.java
@@ -3,10 +3,10 @@ package com.nexus.mail.properties;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Data
-@Configuration
+@Component
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "spring.mail.admin")
 public class MailProperties {
