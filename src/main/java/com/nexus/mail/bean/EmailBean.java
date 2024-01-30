7745dcb6ea1f187245774a81b8dd7dafffe2e551
igorcampos-dev <icampos.developer@yahoo.com>
2024-01-30 00:02:26 -0300

    FIX: correcoes de implementacoes e classes

---
+++ src/main/java/com/nexus/mail/bean/EmailBean.java
@@ -0,0 +1,19 @@
+package com.nexus.mail.bean;
+
+import com.nexus.mail.SendEmailService;
+import com.nexus.mail.properties.MailProperties;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class EmailBean {
+
+    private final MailProperties mailProperties;
+
+    @Bean
+    SendEmailService emailService(){
+        return new SendEmailService(mailProperties);
+    }
+}
