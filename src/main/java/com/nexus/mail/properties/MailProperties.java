db59a1231734e9ca97245244b071f2a922a9ba87
igorcampos-dev <icampos.developer@yahoo.com>
2024-01-31 00:04:10 -0300

    FIX: correcoes de propriedades de email

--- src/main/java/com/nexus/mail/properties/MailProperties.java
+++ src/main/java/com/nexus/mail/properties/MailProperties.java
@@ -1,14 +1,8 @@
 package com.nexus.mail.properties;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Data
-@Component
-@EnableConfigurationProperties
-@ConfigurationProperties(prefix = "spring.mail.admin")
 public class MailProperties {
 
     private String email;
